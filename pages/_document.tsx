50d4c8b2b5e8762bc3f215e6baa1724ecc95a2bd
mschandana16 <chandanasuresh2004@gmail.com>
2024-07-01 00:08:07 +0530

    updates to doc.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,4 @@
-/* import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -18,4 +18,3 @@ export default function Document() {
     </Html>
   );
 }
- */
